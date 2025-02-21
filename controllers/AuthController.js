9d888dcacb9753926ab684f988629f1ea4495414
samya-roy <samyaroy999@gmail.com>
2025-02-21 13:51:29 +0600

    register api created

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -16,3 +16,5 @@ class AuthController {
     }
   }
 }
+
+export default AuthController;
--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+import apiRoutes from "./routes/api.js";
+
+app.use("/api", apiRoutes);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
